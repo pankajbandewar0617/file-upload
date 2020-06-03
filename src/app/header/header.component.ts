39f2d88ed72274b7e979bcd566ad9f25286db265
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2020-06-03 12:41:38 +0530

    design header and footer of the page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  countries = ['IND', 'US'];
+  flags = {
+    IND: 'https://restcountries.eu/data/ind.svg',
+    US: 'https://restcountries.eu/data/usa.svg',
+  };
+  selectedFlag: string = this.flags.US;
 
-  ngOnInit(): void {
+  onSelect(event) {
+    const value = event.target.value;
+    this.selectedFlag = this.flags[value];
   }
-
 }

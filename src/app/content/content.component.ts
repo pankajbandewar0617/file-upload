db982f23de07abbc55be6e30b8d208c45dd30575
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2020-06-05 15:23:06 +0530

    add material datatable with pagination and search

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ShowDataService } from '../show-data.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpEventType } from '@angular/common/http';
-import { compileDirectiveFromMetadata } from '@angular/compiler';
-import { FileUploadComponent } from '../file-upload/file-upload.component';
 import { FileUploadService } from '../file-upload.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+// export interface UserData {
+//   date_closed: string;
+//   product: string;
+//   inquiry: string;
+//   document: string;
+//   action: string;
+//   review: string;
+// }
 
 @Component({
   selector: 'app-content',
@@ -12,13 +21,43 @@ import { FileUploadService } from '../file-upload.service';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  countries: {};
+  jobID: string;
+  extractedData: any;
+
+  // column you want to display
+
+  displayedColumns: string[] = [
+    'name',
+    'capital',
+    'population',
+    'region',
+    'area',
+  ];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
   constructor(private dataService: FileUploadService, private router: Router) {}
 
   ngOnInit() {
-    this.dataService.data$.subscribe((data) => {
-      this.countries = data;
+    this.dataService.data$.subscribe((data: any) => {
+      this.jobID = data[0].jobID;
+      this.extractedData = data[1].extractedData;
+      this.dataSource = new MatTableDataSource(this.extractedData);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   goBack() {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
-const MaterialComponents = [MatToolbarModule, MatButtonModule];
+const MaterialComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatTableModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatSortModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
